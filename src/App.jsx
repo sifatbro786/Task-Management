83075cfff8ede75e20d258f31eb54a7a0c18b69b
sifatbro786 <solaimanislamsifat@gmail.com>
2024-10-13 15:20:03 +0600

    Task Board section design done and i made part by part component for this

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Footer from "./HomePage/Footer";
+import HeroSection from "./HomePage/HeroSection";
 import Navbar from "./HomePage/Navbar";
+import TaskBoard from "./HomePage/Task/TaskBoard";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection/>
+        <TaskBoard/>
+      </div>
       <Footer/>
     </>
   );
