72180cb34f84acb310c63aa43e12ce4a22c73c38
sifatbro786 <solaimanislamsifat@gmail.com>
2024-10-13 14:35:05 +0600

    Hero Section complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Footer from "./HomePage/Footer";
 import Navbar from "./HomePage/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Footer/>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      inter: ["ui-sans-serif", "system-ui"],
+      "inter": ["Inter", "serif"],
     },
 
     extend: {
